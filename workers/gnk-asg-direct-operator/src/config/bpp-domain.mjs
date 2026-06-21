1f23a61eb0854b47e64840596f8ef9fae9a376e9
beckuphome-gnk <beckuphome@gmail.com>
2026-06-22 01:39:44 +0200

    feat(bpp): add backend canonical domain config

---
+++ workers/gnk-asg-direct-operator/src/config/bpp-domain.mjs
@@ -0,0 +1,10 @@
+export const BPP_DOMAIN = Object.freeze({
+  product: 'Bitcoin Payment Processor',
+  host: 'bpp.is',
+  url: 'https://bpp.is/',
+  display: 'BPP.IS'
+});
+
+export function getBppDomainConfig() {
+  return BPP_DOMAIN;
+}
