6d0f734c25a9783b2038145c352950a21c5974cc
beckuphome-gnk <beckuphome@gmail.com>
2026-06-22 01:39:19 +0200

    feat(bpp): add shared frontend domain config

---
+++ packages/config/bpp-domain.mjs
@@ -0,0 +1,9 @@
+export const BPP_PRODUCT_NAME = 'Bitcoin Payment Processor';
+export const BPP_SHORT_NAME = 'BPP';
+export const BPP_CANONICAL_HOST = 'bpp.is';
+export const BPP_CANONICAL_URL = 'https://bpp.is/';
+export const BPP_DISPLAY_DOMAIN = 'BPP.IS';
+
+export function bppPublicLink() {
+  return BPP_CANONICAL_URL;
+}
