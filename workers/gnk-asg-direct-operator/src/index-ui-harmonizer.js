a0d9546dc34178432f9bfbf227953c29d8eb8690
beckuphome-gnk <beckuphome@gmail.com>
2026-06-24 00:44:44 +0200

    Učitaj završni centrirani puni meni V9 na svim javnim stranicama

--- workers/gnk-asg-direct-operator/src/index-ui-harmonizer.js
+++ workers/gnk-asg-direct-operator/src/index-ui-harmonizer.js
@@ -40,11 +40,12 @@ const CONTACT_ASSETS =
   '<script defer src="/assets/contact-quality-v2.js?v=20260623-v2"></script>';
 
 const PUBLIC_ASSETS =
-  '<link rel="stylesheet" href="/assets/brand/gnk-asg-global-layer.css?v=20260624-global-7">' +
-  '<link rel="stylesheet" href="/assets/public-site-unified-v6.css?v=20260624-public-7">' +
-  '<link rel="stylesheet" href="/assets/public-menu-centered-v7.css?v=20260624-menu-center-7">' +
-  '<script defer src="/assets/brand/gnk-asg-global-layer.js?v=20260624-global-7"></script>' +
-  '<script defer src="/assets/public-site-unified-v6.js?v=20260624-public-7"></script>';
+  '<link rel="stylesheet" href="/assets/brand/gnk-asg-global-layer.css?v=20260624-global-9">' +
+  '<link rel="stylesheet" href="/assets/public-site-unified-v6.css?v=20260624-public-9">' +
+  '<link rel="stylesheet" href="/assets/public-menu-centered-v7.css?v=20260624-menu-center-9">' +
+  '<script defer src="/assets/brand/gnk-asg-global-layer.js?v=20260624-global-9"></script>' +
+  '<script defer src="/assets/public-site-unified-v6.js?v=20260624-public-9"></script>' +
+  '<script defer src="/assets/public-menu-final-v9.js?v=20260624-menu-final-9"></script>';
 
 function isBackendPath(path) {
   return BACKEND_PREFIXES.some(prefix =>
@@ -72,7 +73,8 @@ function stripSharedAssets(html) {
     .replace(/<script[^>]+gnk-asg-global-layer\.js[^>]*><\/script>/gi,'')
     .replace(/<link[^>]+public-site-unified-v6\.css[^>]*>/gi,'')
     .replace(/<script[^>]+public-site-unified-v6\.js[^>]*><\/script>/gi,'')
-    .replace(/<link[^>]+public-menu-centered-v7\.css[^>]*>/gi,'');
+    .replace(/<link[^>]+public-menu-centered-v7\.css[^>]*>/gi,'')
+    .replace(/<script[^>]+public-menu-final-v9\.js[^>]*><\/script>/gi,'');
 }
 
 function injectHtml(response, html, assets, cacheControl) {
